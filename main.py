03741172ba49a1a1ece7efa86342c046dd9dc65f
Noel Tanner <noeltanner@gmail.com>
2024-06-24 09:44:09 -0500

    updated to match the bootdev example as it's a bit cleaner

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# print("hello world")
-
-
+# /Users/ntanner/.pyenv/shims/python
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = len(file_contents.split())
-        print(word_count)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
